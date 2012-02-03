317902e8a844391e0b33540165411f24fc8b6948
Alex Lementuev <a.lementuev@gmail.com>
2012-02-03 11:30:54 +0300

    Remove indexer from object

--- CraquaLive/CraquaLive/AsObject.cs
+++ CraquaLive/CraquaLive/AsObject.cs
@@ -9,19 +9,6 @@ namespace flash
 		public static AsObject prototype;
 		public AsObject constructor;
 		
-        public Object this[Object name]
-        {
-            get
-            {
-                // This indexer is very simple, and just returns or sets
-                // the corresponding element from the internal array.
-                return null;
-            }
-            set
-            {                
-            }
-        }
-
 		public bool hasOwnProperty(String name)
 		{
 			return false;
