4162c9ebe8b02ebe0a9004f7d5c9d409a98a52ce
Alex Lementuev <a.lementuev@gmail.com>
2012-02-20 13:23:55 +0300

    Content progress

---
+++ CraquaLive/CraquaLive/Code/Native/bc/flash/resources/BcMusic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bc.flash.resources
+{
+    public class BcMusic : BcManagedResource
+    {
+    }
+}
