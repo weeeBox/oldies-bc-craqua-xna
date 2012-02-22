66c62a9131779b065d1fb708db77b0ea1d695e76
Alex Lementuev <a.lementuev@gmail.com>
2012-02-22 12:31:38 +0300

    Platform progress

--- CraquaLive/CraquaLive/Code/Api/bc/flash/display/AsGraphics.cs
+++ CraquaLive/CraquaLive/Code/Api/bc/flash/display/AsGraphics.cs
@@ -1,14 +1,15 @@
 using System;
  
 using bc.flash;
-using bc.flash.error;
+using bc.flash.display;
  
 namespace bc.flash.display
 {
 	public class AsGraphics : AsObject
 	{
 		public virtual void beginFill(uint color, float alpha)
-		{			
+		{
+			AsBcRenderSupport.beginFill(color, alpha);
 		}
 		public virtual void beginFill(uint color)
 		{
@@ -16,35 +17,35 @@ namespace bc.flash.display
 		}
 		public virtual void clear()
 		{
-			
+			AsBcRenderSupport.clear();
 		}
 		public virtual void curveTo(float controlX, float controlY, float anchorX, float anchorY)
 		{
-			
+			AsBcRenderSupport.curveTo(controlX, controlY, anchorX, anchorY);
 		}
 		public virtual void drawCircle(float x, float y, float radius)
 		{
-			
+			AsBcRenderSupport.drawCircle(x, y, radius);
 		}
 		public virtual void drawEllipse(float x, float y, float width, float height)
 		{
-			
+			AsBcRenderSupport.drawEllipse(x, y, width, height);
 		}
 		public virtual void drawRect(float x, float y, float width, float height)
 		{
-			
+			AsBcRenderSupport.drawRect(x, y, width, height);
 		}
 		public virtual void drawRoundRect(float x, float y, float width, float height, float ellipseWidth, float ellipseHeight)
 		{
-			
+			AsBcRenderSupport.drawRoundRect(x, y, width, height, ellipseWidth, ellipseHeight);
 		}
 		public virtual void endFill()
 		{
-			
+			AsBcRenderSupport.endFill();
 		}
 		public virtual void lineStyle(float thickness, uint color, float alpha, bool pixelHinting, String scaleMode, String caps, String joints, float miterLimit)
 		{
-			
+			AsBcRenderSupport.lineStyle(thickness, color, alpha, pixelHinting, scaleMode, caps, joints, miterLimit);
 		}
 		public virtual void lineStyle(float thickness, uint color, float alpha, bool pixelHinting, String scaleMode, String caps, String joints)
 		{
@@ -80,31 +81,31 @@ namespace bc.flash.display
 		}
 		public virtual void lineTo(float x, float y)
 		{
-			
+			AsBcRenderSupport.lineTo(x, y);
 		}
 		public virtual void moveTo(float x, float y)
 		{
-			
+			AsBcRenderSupport.moveTo(x, y);
 		}
 		public virtual void scale(float scaleX, float scaleY)
 		{
-			throw new AsAbstractClassError();
+			AsBcRenderSupport.scale(scaleX, scaleY);
 		}
 		public virtual void rotate(float a)
 		{
-			throw new AsAbstractClassError();
+			AsBcRenderSupport.rotate(a);
 		}
 		public virtual void translate(float x, float y)
 		{
-			throw new AsAbstractClassError();
+			AsBcRenderSupport.translate(x, y);
 		}
 		public virtual void pushMatrix()
 		{
-			throw new AsAbstractClassError();
+			AsBcRenderSupport.pushMatrix();
 		}
 		public virtual void popMatrix()
 		{
-			throw new AsAbstractClassError();
+			AsBcRenderSupport.popMatrix();
 		}
 	}
 }
---
+++ CraquaLive/CraquaLive/Code/Platform/bc/flash/display/AsBcRenderSupport.cs
@@ -0,0 +1,72 @@
+using System;
+
+namespace bc.flash.display
+{
+	public class AsBcRenderSupport : AsObject
+	{
+		public static void beginFill(uint color, float alpha)
+		{
+			
+		}
+		public static void clear()
+		{
+			
+		}
+		public static void curveTo(float controlX, float controlY, float anchorX, float anchorY)
+		{
+			
+		}
+		public static void drawCircle(float x, float y, float radius)
+		{
+			
+		}
+		public static void drawEllipse(float x, float y, float width, float height)
+		{
+			
+		}
+		public static void drawRect(float x, float y, float width, float height)
+		{
+			
+		}
+		public static void drawRoundRect(float x, float y, float width, float height, float ellipseWidth, float ellipseHeight)
+		{
+			
+		}
+		public static void endFill()
+		{
+			
+		}
+		public static void lineStyle(float thickness, uint color, float alpha, bool pixelHinting, String scaleMode, String caps, String joints, float miterLimit)
+		{
+			
+		}
+		public static void lineTo(float x, float y)
+		{
+			
+		}
+		public static void moveTo(float x, float y)
+		{
+			
+		}
+		public static void scale(float scaleX, float scaleY)
+		{
+			
+		}
+		public static void rotate(float a)
+		{
+			
+		}
+		public static void translate(float x, float y)
+		{
+			
+		}
+		public static void pushMatrix()
+		{
+			
+		}
+		public static void popMatrix()
+		{
+			
+		}
+	}
+}
